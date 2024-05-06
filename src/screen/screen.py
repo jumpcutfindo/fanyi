7852536f13ac2a4c9c7150b693b1636391311123
jumpcutfindo <danielhoe97@gmail.com>
2024-05-06 16:55:09 +0800

    Improve screenshot functions

--- src/screen/screen.py
+++ src/screen/screen.py
@@ -1,8 +1,22 @@
 import mss
+from datetime import datetime
 
 
 def take_full_screenshot():
   sct = mss.mss()
-  filenames = sct.save()
+  filenames = sct.save(
+      mon=-1, output="{dt}_all-monitors".format(dt=__get_screenshot_name()))
 
   return filenames
+
+
+def take_monitor_screenshot(monitor):
+  sct = mss.mss()
+  filenames = sct.save(
+      mon=monitor, output="{dt}_monitor-{mon}".format(dt=__get_screenshot_name(), mon=monitor))
+
+  return filenames
+
+
+def __get_screenshot_name():
+  return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
