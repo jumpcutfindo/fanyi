2bcbaf31e0b9cb347fb824b85c51fec52ffeab38
jumpcutfindo <danielhoe97@gmail.com>
2024-05-06 18:11:51 +0800

    Implement simple input handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,23 @@
+from input import listener
+
+
+def on_request_capture():
+  print('Capture requested')
+
+
+def on_exit(il):
+  print('Exiting application')
+  il.stop()
+
+
 def main():
   print("Initializing application...")
 
-  # Initialization steps here
+  il = listener.InputListener()
+  il.register_hotkey('request_capture', '<ctrl>+<alt>+h', on_request_capture)
+  il.register_hotkey('exit', '<ctrl>+<alt>+j', lambda: on_exit(il))
+
+  il.start()
 
   print("Application initialized")
 
---
+++ src/input/listener.py
@@ -0,0 +1,18 @@
+from pynput import keyboard
+
+
+class InputListener:
+  def __init__(self):
+    self.hotkeys = []
+
+  def start(self):
+    with keyboard.GlobalHotKeys(
+            {x[1]: x[2] for x in self.hotkeys}) as h:
+      self.listener = h
+      h.join()
+
+  def stop(self):
+    keyboard.Listener.stop(self.listener)
+
+  def register_hotkey(self, name: str, combo: str, action):
+    self.hotkeys.append((name, combo, action))
