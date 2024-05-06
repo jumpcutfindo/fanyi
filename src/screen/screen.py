8333efcef7604af5581e5e0ef07ca8799b351a77
jumpcutfindo <danielhoe97@gmail.com>
2024-05-06 16:59:29 +0800

    Fix formatting issue for screenshot name

--- src/screen/screen.py
+++ src/screen/screen.py
@@ -3,20 +3,22 @@ from datetime import datetime
 
 
 def take_full_screenshot():
+  print('Taking full screenshot...')
   sct = mss.mss()
   filenames = sct.save(
-      mon=-1, output="{dt}_all-monitors".format(dt=__get_screenshot_name()))
+      mon=-1, output="{dt}_all-monitors.png".format(dt=__get_screenshot_name()))
 
   return filenames
 
 
 def take_monitor_screenshot(monitor):
+  print('Taking monitor {} screenshot...'.format(monitor))
   sct = mss.mss()
   filenames = sct.save(
-      mon=monitor, output="{dt}_monitor-{mon}".format(dt=__get_screenshot_name(), mon=monitor))
+      mon=monitor, output="{dt}_monitor-{mon}.png".format(dt=__get_screenshot_name(), mon=monitor))
 
   return filenames
 
 
 def __get_screenshot_name():
-  return datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+  return datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
