bd93e97732e7cd41b0a81db13345c63255611180
jumpcutfindo <danielhoe97@gmail.com>
2024-05-07 17:16:15 +0800

    Fix issue with taking full monitor screenshot

--- src/screen/screenshot.py
+++ src/screen/screenshot.py
@@ -6,8 +6,8 @@ from datetime import datetime
 def take_full_screenshot():
   print('Taking full screenshot...')
   sct = mss.mss()
-  filenames = sct.save(
-      mon=-1, output="{dt}_all-monitors.png".format(dt=__get_screenshot_name()))
+  filenames = list(sct.save(
+      mon=-1, output="{dt}_all-monitors.png".format(dt=__get_screenshot_name())))
   print('Took the following screenshot(s): {}'.format(filenames))
 
   return filenames
@@ -23,9 +23,11 @@ def take_monitor_screenshot(monitor):
 
   return filenames
 
+
 def get_monitors():
   sct = mss.mss()
   return sct.monitors
+
 
 def take_partial_screenshot(params):
   print('Taking partial screenshot with params: {}'.format(params))
