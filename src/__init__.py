886762c1428ec3a70a160406ce659027793f35e9
jumpcutfindo <danielhoe97@gmail.com>
2024-05-07 15:25:02 +0800

    Change app to use controller

--- src/__init__.py
+++ src/__init__.py
@@ -1,25 +1,11 @@
-from input import listener
-
-
-def on_request_capture():
-  print('Capture requested')
-
-
-def on_exit(il):
-  print('Exiting application')
-  il.stop()
+from controller import Controller
 
 
 def main():
   print("Initializing application...")
 
-  il = listener.InputListener()
-  il.register_hotkey('request_capture', '<ctrl>+<alt>+h', on_request_capture)
-  il.register_hotkey('exit', '<ctrl>+<alt>+j', lambda: on_exit(il))
-
-  il.start()
-
-  print("Application initialized")
+  controller = Controller()
+  controller.start()
 
 
 if __name__ == "__main__":
