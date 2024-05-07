5a80bfe1f3160a0d4966f272d51828ccbbd9ff40
jumpcutfindo <danielhoe97@gmail.com>
2024-05-07 16:40:02 +0800

    Implement partial screenshot taking

--- src/screen/screenshot.py
+++ src/screen/screenshot.py
@@ -1,4 +1,5 @@
 import mss
+import mss.tools
 from datetime import datetime
 
 
@@ -7,6 +8,7 @@ def take_full_screenshot():
   sct = mss.mss()
   filenames = sct.save(
       mon=-1, output="{dt}_all-monitors.png".format(dt=__get_screenshot_name()))
+  print('Took the following screenshot(s): {}'.format(filenames))
 
   return filenames
 
@@ -17,7 +19,27 @@ def take_monitor_screenshot(monitor):
   filenames = sct.save(
       mon=monitor, output="{dt}_monitor-{mon}.png".format(dt=__get_screenshot_name(), mon=monitor))
 
+  print('Took the following screenshot(s): {}'.format(filenames))
+
   return filenames
+
+def get_monitors():
+  sct = mss.mss()
+  return sct.monitors
+
+def take_partial_screenshot(params):
+  print('Taking partial screenshot with params: {}'.format(params))
+  sct = mss.mss()
+
+  filename = "{dt}_partial.png".format(dt=__get_screenshot_name())
+
+  sct_img = sct.grab(params)
+
+  mss.tools.to_png(sct_img.rgb, sct_img.size, output=filename)
+
+  print('Took the following screenshot(s): {}'.format([filename]))
+
+  return [filename]
 
 
 def __get_screenshot_name():
