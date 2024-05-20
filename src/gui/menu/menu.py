b100e5a6fe67527fc8bb87a50907da6a41c81470
jumpcutfindo <danielhoe97@gmail.com>
2024-05-20 13:43:35 +0800

    Fix invisible separator

--- src/gui/menu/menu.py
+++ src/gui/menu/menu.py
@@ -3,17 +3,19 @@ import tkinter as tk
 from .settings import SettingsFrameContainer
 from .presets import PresetsFrameContainer
 
+
 class MenuFrameContainer:
-  def __init__(self, parent):
-    self.parent = parent
+    def __init__(self, parent):
+        self.parent = parent
 
-    self.frame = tk.Frame(self.parent.frame, width=(self.parent.scale * 280))
-    self.frame.pack(side=tk.LEFT, fill=tk.BOTH, padx=8, pady=8)
-    self.frame.pack_propagate(0)
+        self.frame = tk.Frame(
+            self.parent.frame, width=(self.parent.scale * 280))
+        self.frame.pack(side=tk.LEFT, fill=tk.BOTH, padx=8, pady=8)
+        self.frame.pack_propagate(0)
 
-    self.settings_frame = SettingsFrameContainer(self.parent, self)
+        self.settings_frame = SettingsFrameContainer(self.parent, self)
 
-    sep = tk.Frame(self.frame, width=2, bd=1, relief='sunken')
-    sep.pack(side=tk.TOP, fill=tk.X, padx=8, pady=8)
+        sep = tk.Frame(self.frame, height=2, bd=1, relief='sunken')
+        sep.pack(side=tk.TOP, fill=tk.X, padx=8, pady=16)
 
-    self.presets_frame = PresetsFrameContainer(self.parent, self)
+        self.presets_frame = PresetsFrameContainer(self.parent, self)
