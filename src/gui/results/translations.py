e3eff14cd8c1afc5d962f6c43cbb139f0b8f35b9
jumpcutfindo <danielhoe97@gmail.com>
2024-05-21 15:35:38 +0800

    Adjust translation widget to scale correctly

--- src/gui/results/translations.py
+++ src/gui/results/translations.py
@@ -10,6 +10,13 @@ class TranslationsFrameContainer:
             parent.frame, orient="vertical", command=canvas.yview)
         scrollable_frame = tk.Frame(canvas)
 
+        scrollable_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        canvas.create_window((0, 0), window=scrollable_frame,
+                             anchor=tk.NW, tags=('canvas_frame'))
+        canvas.bind('<Configure>', self.__handle_resize)
+        canvas.configure(yscrollcommand=scrollbar.set)
+
         scrollable_frame.bind(
             "<Configure>",
             lambda e: canvas.configure(
@@ -17,39 +24,28 @@ class TranslationsFrameContainer:
             )
         )
 
-        canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
-        canvas.configure(yscrollcommand=scrollbar.set)
-
         canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.frame = scrollable_frame
 
+    def __handle_resize(self, event):
+        canvas = event.widget
+        canvas_frame = canvas.nametowidget(
+            canvas.itemcget("canvas_frame", "window"))
+        min_width = canvas_frame.winfo_reqwidth()
+        min_height = canvas_frame.winfo_reqheight()
+        if min_width < event.width:
+            canvas.itemconfigure("canvas_frame", width=event.width)
+        if min_height < event.height:
+            canvas.itemconfigure("canvas_frame", height=event.height)
+
+        canvas.configure(scrollregion=canvas.bbox("all"))
+
     def set_translations(self, translations):
         for key, words in translations.items():
             frame = tk.Frame(self.frame, bd=1, relief=tk.SOLID)
-            frame.pack(fill=tk.X, padx=5, pady=5)
+            frame.pack(fill=tk.X, pady=4, expand=True)
 
             key_label = tk.Label(frame, text=key, font=('Arial', 12, 'bold'))
             key_label.pack(anchor=tk.W, padx=5, pady=2)
-
-            for word in words:
-                if not word:
-                    continue
-
-                traditional_label = tk.Label(
-                    frame, text=f"Traditional: {word.traditional}", font=('Arial', 10))
-                traditional_label.pack(anchor=tk.W, padx=20, pady=2)
-
-                simplified_label = tk.Label(
-                    frame, text=f"Simplified: {word.simplified}", font=('Arial', 10))
-                simplified_label.pack(anchor=tk.W, padx=20, pady=2)
-
-                pinyin_label = tk.Label(
-                    frame, text=f"Pinyin: {word.pinyin}", font=('Arial', 10))
-                pinyin_label.pack(anchor=tk.W, padx=20, pady=2)
-
-                for definition in word.definitions:
-                    definition_label = tk.Label(
-                        frame, text=f"Definition: {definition}", font=('Arial', 10))
-                    definition_label.pack(anchor=tk.W, padx=40, pady=2)
