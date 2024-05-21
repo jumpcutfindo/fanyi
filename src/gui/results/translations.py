f884e00fba0cdcbb9dd8c9c781e1132b71edc4b3
jumpcutfindo <danielhoe97@gmail.com>
2024-05-22 00:01:37 +0800

    Remove old translations on screenshot

--- src/gui/results/translations.py
+++ src/gui/results/translations.py
@@ -27,7 +27,7 @@ class TranslationsFrameContainer:
         canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        self.canvas = canvas;
+        self.canvas = canvas
         self.frame = scrollable_frame
 
     def __handle_resize(self, event):
@@ -35,7 +35,7 @@ class TranslationsFrameContainer:
         canvas_frame = canvas.nametowidget(
             canvas.itemcget("canvas_frame", "window"))
         canvas.itemconfigure("canvas_frame", width=event.width)
-        
+
         min_height = canvas_frame.winfo_reqheight()
         if min_height < event.height:
             canvas.itemconfigure("canvas_frame", height=event.height)
@@ -43,6 +43,9 @@ class TranslationsFrameContainer:
         canvas.configure(scrollregion=canvas.bbox("all"))
 
     def set_translations(self, translations):
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+
         for index, (key, entries) in enumerate(translations.items()):
             containing_frame = tk.Frame(self.frame, background='white')
             containing_frame.pack(fill=tk.X, pady=8, padx=8, expand=True)
@@ -59,28 +62,35 @@ class TranslationsFrameContainer:
             if not entries or len(entries) == 0:
                 not_exists_label = tk.Label(
                     translation_frame, text='No words found', font='Arial 10 italic', background='white')
-                not_exists_label.grid(row=1, column=0, padx=8, pady=8, sticky=tk.W)
+                not_exists_label.grid(
+                    row=1, column=0, padx=8, pady=8, sticky=tk.W)
                 continue
-            
+
             # Insert each entry as a line
             for index, entry in enumerate(entries):
                 if not entry:
                     continue
 
-                simplified_label = tk.Label(translation_frame, text=f'{entry.simplified}', font=('Arial', 10), background='white')
-                simplified_label.grid(row=index+1, column=0, padx=(0, 8), sticky=tk.NW)
+                simplified_label = tk.Label(translation_frame, text=f'{entry.simplified}', font=(
+                    'Arial', 10), background='white')
+                simplified_label.grid(
+                    row=index+1, column=0, padx=(0, 8), sticky=tk.NW)
 
-                traditional_label = tk.Label(translation_frame, text=f'({entry.traditional})', font=('Arial', 10), background='white')
-                traditional_label.grid(row=index+1, column=1, padx=8, sticky=tk.NW)
+                traditional_label = tk.Label(translation_frame, text=f'({entry.traditional})', font=(
+                    'Arial', 10), background='white')
+                traditional_label.grid(
+                    row=index+1, column=1, padx=8, sticky=tk.NW)
 
-                pinyin_label = tk.Label(translation_frame, text=f'{entry.pinyin}', font=('Arial', 10), background='white')
+                pinyin_label = tk.Label(translation_frame, text=f'{entry.pinyin}', font=(
+                    'Arial', 10), background='white')
                 pinyin_label.grid(row=index+1, column=2, padx=8, sticky=tk.NW)
 
                 definitions = []
                 for i, d in enumerate(entry.definitions):
                     definitions.append(f'{i+1}. {d}')
-                definitions_label = tk.Label(translation_frame, font=('Arial', 10), text='\n'.join(definitions), background='white', justify='left')                
+                definitions_label = tk.Label(translation_frame, font=(
+                    'Arial', 10), text='\n'.join(definitions), background='white', justify='left')
                 # self.canvas.bind('<Configure>', lambda e: definitions_label.configure(
                 #     wraplength=pinyin_label.winfo_width()), add=True)
-                definitions_label.grid(row=index+1, column=3, padx=8, pady=(0, 24), sticky=tk.W)
-                
+                definitions_label.grid(
+                    row=index+1, column=3, padx=8, pady=(0, 24), sticky=tk.W)
