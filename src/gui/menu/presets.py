da8c33ab1e2d1bf8e8c3e1f43914617ce15464e1
jumpcutfindo <danielhoe97@gmail.com>
2024-05-21 14:24:24 +0800

    Improve layout of preset section

--- src/gui/menu/presets.py
+++ src/gui/menu/presets.py
@@ -16,10 +16,12 @@ class PresetsFrameContainer:
             f'{key}: {value["width"]}x{value["height"]}': value for key, value in screen_info.items()}
 
         self.frame = tk.Frame(self.parent.frame)
-        self.frame.pack(side=tk.TOP, fill=tk.BOTH, expand=False)
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.rowconfigure(4, weight=1)
+        self.frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         presets_label = tk.Label(self.frame, text="Presets")
-        presets_label.pack(side=tk.TOP, pady=[0, 8], anchor=tk.NW)
+        presets_label.grid(row=0, column=0, pady=8, sticky=tk.W)
 
         self.__preset_config_section()
         self.__preset_list_section()
@@ -42,75 +44,79 @@ class PresetsFrameContainer:
     def __preset_config_section(self):
         # Preset configuration frame
         self.preset_config_frame = tk.Frame(self.frame)
-        self.preset_config_frame.pack(side=tk.TOP, fill=tk.X)
+        self.preset_config_frame.grid(row=1, column=0, sticky=tk.NSEW)
         self.preset_config_frame.grid_columnconfigure(1, weight=1)
 
         # Preset name configuration
         preset_name_label = tk.Label(self.preset_config_frame, text="Name:")
-        preset_name_label.grid(row=0, column=0, sticky=tk.W, pady=2)
+        preset_name_label.grid(
+            row=0, column=0, sticky=tk.W, padx=(0, 16), pady=8)
 
         self.preset_name_var = tk.StringVar(self.root.frame)
         self.preset_name_var.set("New preset")
         preset_name_input = tk.Entry(
             self.preset_config_frame, textvariable=self.preset_name_var)
-        preset_name_input.grid(row=0, column=1, sticky=tk.NSEW, padx=2, pady=2)
+        preset_name_input.grid(row=0, column=1, sticky=tk.NSEW)
 
         # Screen configuration
         screen_value_label = tk.Label(self.preset_config_frame, text="Screen:")
-        screen_value_label.grid(row=1, column=0, sticky=tk.W, pady=2)
+        screen_value_label.grid(
+            row=1, column=0, sticky=tk.W, padx=(0, 16), pady=8)
 
         self.screen_value_var = tk.StringVar(self.root.frame)
         screen_dropdown = tk.OptionMenu(self.preset_config_frame, self.screen_value_var,
                                         *self.screen_display_names, command=self.__on_screen_selected)
-        screen_dropdown.grid(row=1, column=1, sticky=tk.W, padx=2, pady=2)
+        screen_dropdown.grid(row=1, column=1, sticky=tk.NSEW, pady=8)
 
         # Dimensions configuration (top, left, height, width)
         self.dimension_values_frame = tk.Frame(self.frame)
-        self.dimension_values_frame.pack(side=tk.TOP, fill=tk.X, pady=2)
+        self.dimension_values_frame.columnconfigure([1, 3, 5, 7], weight=1)
+        self.dimension_values_frame.grid(
+            row=2, column=0, pady=8, sticky=tk.NSEW)
 
         self.left_value_var = tk.IntVar(self.root.frame)
         self.left_value_var.set(0)
         left_label = tk.Label(self.dimension_values_frame, text="L:")
-        left_label.pack(side=tk.LEFT)
+        left_label.grid(row=0, column=0, padx=[0, 8], sticky=tk.NSEW)
         left_value_input = tk.Entry(
             self.dimension_values_frame, textvariable=self.left_value_var, width=5)
-        left_value_input.pack(side=tk.LEFT, padx=[0, 4])
+        left_value_input.grid(row=0, column=1, padx=8, sticky=tk.NSEW)
 
         self.top_value_var = tk.IntVar(self.root.frame)
         self.top_value_var.set(0)
         top_label = tk.Label(self.dimension_values_frame, text="T:")
-        top_label.pack(side=tk.LEFT)
+        top_label.grid(row=0, column=2, padx=[0, 8], sticky=tk.NSEW)
         top_value_input = tk.Entry(
             self.dimension_values_frame, textvariable=self.top_value_var, width=5)
-        top_value_input.pack(side=tk.LEFT, padx=[0, 4])
+        top_value_input.grid(row=0, column=3, padx=8, sticky=tk.NSEW)
 
         self.width_value_var = tk.IntVar(self.root.frame)
         self.width_value_var.set(0)
         width_label = tk.Label(self.dimension_values_frame, text="W:")
-        width_label.pack(side=tk.LEFT)
+        width_label.grid(row=0, column=4, padx=[0, 8], sticky=tk.NSEW)
         width_value_input = tk.Entry(
             self.dimension_values_frame, textvariable=self.width_value_var, width=5)
-        width_value_input.pack(side=tk.LEFT, padx=[0, 4])
+        width_value_input.grid(row=0, column=5, padx=8, sticky=tk.NSEW)
 
         self.height_value_var = tk.IntVar(self.root.frame)
         self.height_value_var.set(0)
         height_label = tk.Label(self.dimension_values_frame, text="H:")
-        height_label.pack(side=tk.LEFT)
+        height_label.grid(row=0, column=6, padx=[0, 8], sticky=tk.NSEW)
         height_value_input = tk.Entry(
             self.dimension_values_frame, textvariable=self.height_value_var, width=5)
-        height_value_input.pack(side=tk.LEFT, padx=[0, 4])
+        height_value_input.grid(row=0, column=7, padx=8, sticky=tk.NSEW)
 
         # Preset controls
         self.preset_controls_frame = tk.Frame(self.frame)
-        self.preset_controls_frame.pack(side=tk.TOP, fill=tk.X, pady=2)
+        self.preset_controls_frame.grid(row=3, column=0, sticky=tk.EW)
 
         self.delete_preset_button = tk.Button(
             self.preset_controls_frame, text="Delete", command=self.__on_delete_preset)
-        self.delete_preset_button.pack(side=tk.RIGHT, padx=2)
+        self.delete_preset_button.pack(side=tk.RIGHT, padx=8)
 
         self.save_preset_button = tk.Button(
             self.preset_controls_frame, text="Save", command=self.__on_save_preset)
-        self.save_preset_button.pack(side=tk.RIGHT, padx=2)
+        self.save_preset_button.pack(side=tk.RIGHT, padx=8)
 
         # Set default screen selection
         self.screen_value_var.set(self.screen_display_names[0])
@@ -118,8 +124,7 @@ class PresetsFrameContainer:
 
     def __preset_list_section(self):
         self.preset_list_frame = tk.Frame(self.frame)
-        self.preset_list_frame.pack(
-            side=tk.TOP, fill=tk.BOTH, pady=[0, 8], expand=True)
+        self.preset_list_frame.grid(row=4, column=0, sticky=tk.NSEW, pady=8)
 
         self.preset_list_box = tk.Listbox(
             self.preset_list_frame, selectmode=tk.SINGLE)
@@ -130,11 +135,13 @@ class PresetsFrameContainer:
 
     def __preset_controls_section(self):
         self.preset_controls_frame = tk.Frame(self.frame)
-        self.preset_controls_frame.pack(side=tk.TOP, fill=tk.X)
+        self.preset_controls_frame.grid(
+            row=5, column=0, pady=8, sticky=tk.NSEW)
+        self.preset_controls_frame.grid_columnconfigure(0, weight=1)
 
         screenshot_control = tk.Button(
             self.preset_controls_frame, text="Screenshot", command=self.__on_screenshot)
-        screenshot_control.pack(side=tk.TOP, fill=tk.X)
+        screenshot_control.grid(row=0, column=0, sticky=tk.NSEW)
 
     def __on_screen_selected(self, screen_name):
         logger.debug(f'User action: Selected screen {screen_name}')
